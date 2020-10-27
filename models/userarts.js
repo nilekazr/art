97d7799f9465de9f7e6f1e2a830ccfcf00c5bb41
trefynwynd <jl.alejandria@gmail.com>
2020-10-26 22:06:41 -0700

    copied the header portion from index to header, and updated white space in userarts from previous merge.

--- models/userarts.js
+++ models/userarts.js
@@ -11,10 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-<<<<<<< HEAD
-=======
       
->>>>>>> submain
     }
   };
   userArts.init({
