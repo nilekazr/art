19f17c042c1384300e61236f6b2685e56406c7b3
dcblair <devin.c.blair@gmail.com>
2020-10-28 01:03:33 -0700

    connect api to artist page. render artist page with info. improve tomorrow.

--- routes/art.js
+++ routes/art.js
@@ -2,10 +2,10 @@
 // requirements
 const express = require('express');
 const app = express();
-const db = require('../models');
 const router = express.Router();
 const axios = require('axios');
 const layouts = require('express-ejs-layouts');
+const db = require('../models');
 
 // middleware
 app.set('view engine', 'ejs');
@@ -14,19 +14,25 @@ app.use(express.urlencoded({ extended: false }));
 
 // routes
 
-router.post('/', function(req, res) {
+router.post('/artists', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  console.log(req.body.name)
+  // console.log(req.body.name)
   db.art.findOrCreate({
     where: {
     url: req.body.name,
     }
+  }).then(() => {
+    res.redirect('artists')
   })
-  .then(() => {
-    res.redirect('/artists')
-    })
 });
 
+router.get('/artists/', function(req,res){
+  console.log(req.query.url)
+  let wikiUrl = `https://www.wikiart.org/en/${req.query.name}/?json=2`
+  axios.get(wikiUrl).then((apiResponse) => {
+    res.render('artists', {artist: apiResponse.data});
+  })
+});
 
 router.get('/paintings', function(req, res){
   let randomNum = Math.floor(Math.random() * 10)
@@ -45,15 +51,6 @@ router.get('/paintings/:id', function(req, res) {
 
 router.post('/paintings', function(req, res){
   res.redirect("paintings");
-});
-
-router.get('/artists', function(req,res){
-  let artistUrl = 'pablo-picasso'
-  let wikiUrl = `https://www.wikiart.org/en/${artistUrl}/?json=2`
-  axios.get(wikiUrl).then( function(apiResponse) {
-    res.render('index', {artist: apiResponse.data});
-  })
-  res.render('artists');
 });
 
 
